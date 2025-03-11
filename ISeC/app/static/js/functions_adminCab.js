2bd554d4ba8b7af6cd438c268ea9c18bb0d66041
apakufai <135946094+apakufai@users.noreply.github.com>
2025-03-11 12:25:40 +0300

    Add files via upload

--- ISeC/app/static/js/functions_adminCab.js
+++ ISeC/app/static/js/functions_adminCab.js
@@ -11,7 +11,7 @@ function cab_generateRandomID(prefix, length) {
 
 // Создание id кода доступа с проверкой на наличие в базе данных
 async function cab_generateUniqueCodeId() {
-    let isCodeIdFree = false; // Счётчик проверки на существование id в базе
+    let isIdFree = false; // Счётчик проверки на существование id в базе
     let IdToCheck; // Объявляем переменную для ID
 
     do {
@@ -31,9 +31,9 @@ async function cab_generateUniqueCodeId() {
         }
         if (data.found) {
         } else {
-            isCodeIdFree = true; // ID свободен
+            isIdFree = true; // ID свободен
         }
-    } while (!isCodeIdFree);
+    } while (!isIdFree);
 
     return IdToCheck; // Возвращаем уникальный ID
 }
@@ -53,7 +53,7 @@ async function createCode(button) {
     const startDate = row.querySelector('input[name="start_date"]').value;
     const endDate = row.querySelector('input[name="end_date"]').value;
     // Ожидание результата проверки вводимых данных
-    const { hasErrors, errorList } = await validateCodeInputs("create", codeId, testGroup, testGroupPart2, code, startDate, endDate);
+    const { hasErrors, errorList } = await validateInputs("create", codeId, testGroup, testGroupPart2, code, startDate, endDate);
     if (hasErrors) {
         alert(errorList.join('\n'));  // Выводим ошибки
     } else {
@@ -95,11 +95,11 @@ async function updateCode(button) {
     const startDate = row.querySelector('input[name="start_date"]').value;
     const endDate = row.querySelector('input[name="end_date"]').value;
     // Ожидание результата проверки вводимых данных
-    const { hasErrors, errorList } = await validateCodeInputs("upadte", codeId, testGroup, testGroupPart2, code, startDate, endDate);
+    const { hasErrors, errorList } = await validateInputs("upadte", codeId, testGroup, testGroupPart2, code, startDate, endDate);
     if (hasErrors) {
         alert(errorList.join('\n'));  // Выводим ошибки
     } else {
-        if (confirmUpdateCode(testGroup)) {
+        if (confirmUpdate(testGroup)) {
             fetch(`/cab_update_code/${codeId}`, {
                 method: 'POST',
                 headers: {
@@ -128,7 +128,7 @@ async function updateCode(button) {
 }
 
 // Функция проверки данных на валидность
-async function validateCodeInputs(action, codeId, testGroup, testGroupPart2, code, startDate, endDate) {
+async function validateInputs(action, codeId, testGroup, testGroupPart2, code, startDate, endDate) {
     let hasErrors = false;
     let errorList = [];
     // Проверка на пустое название группы
@@ -194,7 +194,7 @@ function deleteCode(button) {
     const codeId = button.getAttribute('code_id');
     const testGroup = button.getAttribute('test_group');
 
-    if (confirmDeleteCode(testGroup)) {
+    if (confirmDelete(testGroup)) {
         fetch(`/cab_delete_code/${codeId}`, {
             method: 'POST',
             headers: {
@@ -234,13 +234,13 @@ function deleteCode(button) {
 
 // Создание id администратора с проверкой на наличие в базе данных
 async function cab_generateUniqueAdminId() {
-    let isAdminIdFree = false; // Счётчик проверки на существование id в базе
+    let isLoginFree = false; // Счётчик проверки на существование id в базе
     let loginToCheck; // Объявляем переменную для ID
 
     do {
         loginToCheck = cab_generateRandomID("admin_", 4); // Сгенерированный id
         // Отправляем запрос на сервер для проверки id
-        const response = await fetch('/cab_check_admin_id', {
+        const response = await fetch('/cab_check_admin_login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -254,210 +254,9 @@ async function cab_generateUniqueAdminId() {
         }
         if (data.found) {
         } else {
-            isAdminIdFree = true; // ID свободен
+            isLoginFree = true; // ID свободен
         }
-    } while (!isAdminIdFree);
+    } while (!isLoginFree);
 
     return loginToCheck; // Возвращаем уникальный ID
 }
-
-
-
-
-
-// Создание строк таблицы администраторов
-async function createAdmin(button) {
-    const row = button.closest('tr');
-    const adminId = await cab_generateUniqueAdminId();
-    const login = row.querySelector('input[name="admin_login"]').value.trim();
-    const password = row.querySelector('input[name="admin_password"]').value.trim();
-    const adminName = row.querySelector('input[name="admin_name"]').value.trim();
-    const archiveAccess = row.querySelector('input[name="archive_access"]').checked;
-    const codesAccess = row.querySelector('input[name="codes_access"]').checked;
-    const analysisAccess = row.querySelector('input[name="analysis_access"]').checked;
-    const excelgenAccess = row.querySelector('input[name="excelgen_access"]').checked;
-    const startDate = row.querySelector('input[name="start_date"]').value;
-    const endDate = row.querySelector('input[name="end_date"]').value;
-    // Ожидание результата проверки вводимых данных
-    const { hasErrors, errorList } = await validateAdminInputs("create", adminId, login, password, adminName, startDate, endDate);
-    if (hasErrors) {
-        alert(errorList.join('\n'));  // Выводим ошибки
-    } else {
-        fetch('/cab_create_code', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                admin_id: adminId,
-                admin_login: login,
-                admin_password: password,
-                admin_name: adminName,
-                archive_access: archiveAccess,
-                codes_access: codesAccess,
-                analysis_access: analysisAccess,
-                excelgen_access: excelgenAccess,
-                start_date: startDate,
-                end_date: endDate
-            }),
-            credentials: 'same-origin'  // Защита запросов от CSRF-атак
-        })
-            .then(response => {
-                if (response.ok) {
-                    location.reload();
-                } else {
-                    alert('Ошибка при создании');
-                }
-            })
-            .catch(error => {
-                console.error('Ошибка:', error);
-            });
-    }
-}
-
-
-
-// // Изменение строк таблицы кодов доступа
-// async function updateAdmin(button) {
-//     const row = button.closest('tr');
-
-//     const login = row.querySelector('input[name="admin_login"]').value.trim();
-//     const password = row.querySelector('input[name="admin_password"]').value.trim();
-//     const adminName = row.querySelector('input[name="admin_name"]').value.trim();
-//     const archiveAccess = row.querySelector('input[name="archive_access"]').checked;
-//     const codesAccess = row.querySelector('input[name="codes_access"]').checked;
-//     const analysisAccess = row.querySelector('input[name="analysis_access"]').checked;
-//     const excelgenAccess = row.querySelector('input[name="excelgen_access"]').checked;
-//     const startDate = row.querySelector('input[name="start_date"]').value;
-//     const endDate = row.querySelector('input[name="end_date"]').value;
-
-
-//     const login = button.getAttribute('admin_id');
-//     const testGroupPart1 = row.querySelector('.entrycode_block_td_0').textContent.trim();
-//     const testGroupPart2 = row.querySelector('input[name="test_group"]').value.trim();
-//     const testGroup = `${testGroupPart1} ${testGroupPart2}`;
-//     const code = row.querySelector('input[name="code"]').value.trim();
-//     const startDate = row.querySelector('input[name="start_date"]').value;
-//     const endDate = row.querySelector('input[name="end_date"]').value;
-
-
-//     // Ожидание результата проверки вводимых данных
-//     const { hasErrors, errorList } = await validateAdminInputs("upadte", codeId, testGroup, testGroupPart2, code, startDate, endDate);
-//     if (hasErrors) {
-//         alert(errorList.join('\n'));  // Выводим ошибки
-//     } else {
-//         if (confirmUpdateAdmin(testGroup)) {
-//             fetch(`/cab_update_code/${codeId}`, {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 },
-//                 body: JSON.stringify({
-//                     test_group: testGroup,
-//                     code: code,
-//                     start_date: startDate,
-//                     end_date: endDate
-//                 }),
-//                 credentials: 'same-origin'  // Защита запросов от CSRF-атак
-//             })
-//                 .then(response => {
-//                     if (response.ok) {
-//                         location.reload();
-//                     } else {
-//                         alert('Ошибка при обновлении');
-//                     }
-//                 })
-//                 .catch(error => {
-//                     console.error('Ошибка:', error);
-//                 });
-//         }
-//     }
-// }
-
-// // Функция проверки данных на валидность
-// async function validateAdminInputs(action, adminId, login, password, adminName, startDate, endDate) {
-//     let hasErrors = false;
-//     let errorList = [];
-//     // Проверка на пустое название группы
-//     if (!testGroupPart2) {
-//         hasErrors = true;
-//         errorList.push("Название группы не должно быть пустым");
-//     }
-//     // Проверка на пустой код
-//     if (!code) {
-//         hasErrors = true;
-//         errorList.push("Поле кода доступа не должно быть пустым");
-//     }
-//     // Проверка на существование ДАННЫЕ АДМИНИСТРАТОРА!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! в базе данных
-//     try {
-//         const response = await fetch('/cab_check_admin', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({ action: action, codeId: codeId, testGroup: testGroup, code: code }),
-//         });
-//         if (!response.ok) {
-//             throw new Error('Ошибка сети');
-//         }
-//         const checkResult = await response.json();
-//         if (checkResult.isGroupInDB) {
-//             hasErrors = true;
-//             errorList.push("Такая группа уже существует");
-//         }
-//         if (checkResult.isCodeInDB) {
-//             hasErrors = true;
-//             errorList.push("Поле кода доступа уже существует");
-//         }
-//     } catch (error) {
-//         console.error('Ошибка:', error);
-//         hasErrors = true;
-//         errorList.push("Ошибка проверки данных в базе");
-//     }
-//     // Проверка на пустые поля дат
-//     if (!startDate) {
-//         hasErrors = true;
-//         errorList.push("Поле ввода даты ОТ пустое либо некорректное");
-//     }
-//     if (!endDate) {
-//         hasErrors = true;
-//         errorList.push("Поле ввода даты ДО пустое либо некорректное");
-//     }
-//     // Проверка на корректность дат
-//     if (new Date(startDate) > new Date(endDate)) {
-//         hasErrors = true;
-//         errorList.push("Дата ОТ позднее, чем дата ДО");
-//     }
-//     if (new Date(endDate) < new Date()) {
-//         hasErrors = true;
-//         errorList.push("Дата ДО уже прошла");
-//     }
-//     // Возвращаем результат проверки и список ошибок
-//     return { hasErrors, errorList };
-// }
-
-// // Удаление строк таблицы кодов доступа
-// function deleteAdmin(button) {
-//     const codeId = button.getAttribute('code_id');
-//     const testGroup = button.getAttribute('test_group');
-
-//     if (confirmDeleteAdmin(testGroup)) {
-//         fetch(`/cab_delete_code/${codeId}`, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             credentials: 'same-origin'  // Защита запросов от CSRF-атак
-//         })
-//             .then(response => {
-//                 if (response.ok) {
-//                     location.reload();
-//                 } else {
-//                     alert('Ошибка при удалении');
-//                 }
-//             })
-//             .catch(error => {
-//                 console.error('Ошибка:', error);
-//             });
-//     }
-// }

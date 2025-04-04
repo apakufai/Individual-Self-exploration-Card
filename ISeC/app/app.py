87445c13990a2902899b21432fbc910efe80971c
Anton Anisimov <av_anisimov@betboom.org>
2025-04-04 13:08:10 +0300

    -> add | pre test tmp

--- ISeC/app/app.py
+++ ISeC/app/app.py
@@ -33,17 +33,26 @@ from openpyxl.utils import get_column_letter
 
 from flaskext.mysql import MySQL
 
-# log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
-# log_file = "www/isec.smart-skills.pro/log"
-# my_handler = RotatingFileHandler(log_file, mode='a', maxBytes=5*1024*1024, backupCount=2, encoding=None, delay=0)
-# my_handler.setFormatter(log_formatter)
-# my_handler.setLevel(logging.INFO)
-# app_log = logging.getLogger("root")
-# app_log.setLevel(logging.INFO)
-# app_log.addHandler(my_handler)
+log_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+my_handler = RotatingFileHandler(
+    os.environ["LOGFILE_PATH"],
+    mode='a',
+    maxBytes=5*1024*1024,
+    backupCount=2,
+    encoding=None,
+    delay=0
+)
+my_handler.setFormatter(log_formatter)
+my_handler.setLevel(logging.INFO)
+log = logging.getLogger("root")
+log.setLevel(logging.INFO)
+log.addHandler(my_handler)
+
 
 
 mysql = MySQL()
+
+log.info("Init")
 
 application = Flask(__name__)
 application.config["MYSQL_DATABASE_USER"] = os.environ["MYSQL_DATABASE_USER"]
